1bbf8cd90682831a96580837b9976d23e0202c3b
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2011-02-16 13:44:08 +0100

    drbd: added support for RedHat 6

--- manifests/classes/drbd.pp
+++ manifests/classes/drbd.pp
@@ -16,46 +16,97 @@ class drbd::base {
 
     RedHat: {
 
-      if ( ! $centos_mirror ) {
-        $centos_mirror = "http://mirror.switch.ch/ftp/mirror/centos/"
+      case $lsbmajdistrelease {
+        "6": {
+          # Note: as CentOS 6 has not yet been released, we can't fetch drbd
+          # packages for RHEL 6 from there. This recipe fetches them from
+          # ATrpms. Maybe we can remove this differenciation once CentOS 6 is
+          # released.
+
+          yumrepo { "atrpms-drbd":
+            descr => "DRBD packages from ATrpms for RHEL ${lsbmajdistrelease}",
+	    baseurl => "http://dl.atrpms.net/el6-${architecture}/atrpms/stable",
+            enabled => 1,
+            gpgkey => "http://packages.atrpms.net/RPM-GPG-KEY.atrpms",
+            gpgcheck => 1,
+            includepkgs => "drbd,drbd-kmdl-2.6.32-71.14.1.el6.x86_64",
+          }
+
+          # ensure file is managed in case we want to purge /etc/yum.repos.d/
+          # http://projects.puppetlabs.com/issues/3152
+          file { "/etc/yum.repos.d/atrpms-drbd.repo":
+            ensure  => present,
+            mode    => 0644,
+            owner   => "root",
+            require => Yumrepo["atrpms-drbd"],
+          }
+
+          if $virtual == "xenu" {
+            fail "DRDB on a XEN instance not supported with RHEL6 yet, sorry."
+          }
+
+          package { "drbd":
+            ensure  => present,
+            require => Yumrepo["atrpms-drbd"],
+          }
+    
+          package { "drbd-kmdl-2.6.32-71.14.1.el6.x86_64":
+            ensure  => present,
+            alias   => "drbd-module",
+            require => Yumrepo["atrpms-drbd"],
+          }
+
+          # Should probably be created by the drbd package, but is not.
+          file { "/var/lib/drbd":
+            ensure => directory,
+          }
+
+        }
+        default: {
+
+          if ( ! $centos_mirror ) {
+            $centos_mirror = "http://mirror.switch.ch/ftp/mirror/centos/"
+          }
+
+          yumrepo { "centos-extra-drbd":
+            descr => "DRBD packages from Centos-extras for RHEL ${lsbmajdistrelease}",
+            baseurl => "${centos_mirror}${operatingsystemrelease}/extras/${architecture}/",
+            enabled => 1,
+            gpgkey => "${centos_mirror}/RPM-GPG-KEY-CentOS-${lsbmajdistrelease}",
+            gpgcheck => 1,
+            includepkgs => "drbd83,kmod-drbd83,kmod-drbd83-xen",
+          }
+
+          # ensure file is managed in case we want to purge /etc/yum.repos.d/
+          # http://projects.puppetlabs.com/issues/3152
+          file { "/etc/yum.repos.d/centos-extra-drbd.repo":
+            ensure  => present,
+            mode    => 0644,
+            owner   => "root",
+            require => Yumrepo["centos-extra-drbd"],
+          }
+
+          if $virtual == "xenu" {
+            $kmodpkg = "kmod-drbd83-xen"
+          } else {
+            $kmodpkg = "kmod-drbd83"
+          }
+
+          package { "drbd83":
+            ensure  => present,
+            alias   => "drbd",
+            require => Yumrepo["centos-extra-drbd"],
+          }
+    
+          package { $kmodpkg:
+            ensure  => present,
+            alias   => "drbd-module",
+            require => Yumrepo["centos-extra-drbd"],
+          }
+
+        }
       }
 
-      yumrepo { "centos-extra-drbd":
-        descr => "DRBD packages from Centos-extras for RHEL ${lsbmajdistrelease}",
-        baseurl => "${centos_mirror}${operatingsystemrelease}/extras/${architecture}/",
-        enabled => 1,
-        gpgkey => "${centos_mirror}/RPM-GPG-KEY-CentOS-${lsbmajdistrelease}",
-        gpgcheck => 1,
-        includepkgs => "drbd83,kmod-drbd83,kmod-drbd83-xen",
-      }
-
-      # ensure file is managed in case we want to purge /etc/yum.repos.d/
-      # http://projects.puppetlabs.com/issues/3152
-      file { "/etc/yum.repos.d/centos-extra-drbd.repo":
-        ensure  => present,
-        mode    => 0644,
-        owner   => "root",
-        require => Yumrepo["centos-extra-drbd"],
-      }
-
-
-      if $virtual == "xenu" {
-        $kmodpkg = "kmod-drbd83-xen"
-      } else {
-        $kmodpkg = "kmod-drbd83"
-      }
-
-      package { "drbd83":
-        ensure  => present,
-        alias   => "drbd",
-        require => Yumrepo["centos-extra-drbd"],
-      }
-
-      package { $kmodpkg:
-        ensure  => present,
-        alias   => "drbd-module",
-        require => Yumrepo["centos-extra-drbd"],
-      }
 
     }
 
